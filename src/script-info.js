263dff136dad7a7614677985ab1e0f63c8b533c2
Netoxic <35343567+Netoxique@users.noreply.github.com>
2026-08-27 22:39:22 -0600

    Bump Melonbooks Enhanced to 1.0.2

--- src/script-info.js
+++ src/script-info.js
@@ -1,7 +1,7 @@
 export class ScriptInfo {
   static name = 'Melonbooks - Enhancements';
   static namespace = 'https://github.com/Netoxique/Melonbooks-Enhanced';
-  static version = '1.0.1';
+  static version = '1.0.2';
   static description = 'Comprehensive enhancements for Melonbooks browsing, shopping, layout, and library management.';
   static author = 'Netoxique';
 }
