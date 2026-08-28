ac41116db2297ec50c5e7867a47b2843b1d950ac
Netoxic <35343567+Netoxique@users.noreply.github.com>
2026-08-27 21:53:36 -0600

    Correct userscript repository metadata

--- src/script-info.js
+++ src/script-info.js
@@ -1,7 +1,7 @@
 export class ScriptInfo {
   static name = 'Melonbooks - Enhancements';
-  static namespace = 'https://github.com/Netoxic/melonbooks-enhancements';
+  static namespace = 'https://github.com/Netoxique/Melonbooks-Enhanced';
   static version = '1.0.1';
   static description = 'Comprehensive enhancements for Melonbooks browsing, shopping, layout, and library management.';
-  static author = 'Netoxic';
+  static author = 'Netoxique';
 }
